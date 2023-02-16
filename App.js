5d35c88e149af1c969b7f2104b07d2037f93703e
volodya <v.khryst@outlook.com>
2023-02-16 14:58:58 -0500

    added damage calculator and 1 sigma calculation

--- App.js
+++ App.js
@@ -3,36 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import Slider from '@react-native-community/slider';
 import React, {useState} from 'react';
 import DiceSlider from './diceSlider';
+import DamageCalculator from './damageCalculator'
 
 export default function App() {
-    const [attacks, setAttacks] = useState(0);
-    const [hitChance, setHitChance] = useState(3);
-    const [woundChance, setWoundChance] = useState(3);
-    const [saveChance, setSaveChance] = useState(3);
+    const [attacks, setAttacks] = useState(1);
+    const [hitChance, setHitChance] = useState(1);
+    const [woundChance, setWoundChance] = useState(1);
+    const [saveChance, setSaveChance] = useState(1);
   return (
     <View style={styles.container}>
-      <Text>Number of attacks:</Text>
+      <Text>Number of attacks: {attacks}</Text>
       <DiceSlider max={40} updater={setAttacks}/>
-          <Text>
-              {attacks}
-          </Text>
-      <Text>Needed to roll to hit:</Text>
+
+          <Text>Needed to roll to hit: {hitChance}+</Text>
           <DiceSlider max={6} updater={setHitChance} />
-          <Text>
-              {hitChance}
-          </Text>
 
-          <Text>Default wound roll:</Text>
+          <Text>Default wound roll: {woundChance}+</Text>
           <DiceSlider max={6} updater={setWoundChance} />
-          <Text>
-              {woundChance}
-          </Text>
 
-          <Text>Opponents default saving roll:</Text>
-          <DiceSlider max={6} updater={setSaveChance} />
-          <Text>
-              {saveChance}
-          </Text>
+          <Text>Opponents default saving roll: {saveChance}+</Text>
+          <DiceSlider max={7} updater={setSaveChance} />
+          <DamageCalculator attacks={attacks} hitchance={hitChance} woundchance={woundChance} savechance={saveChance}/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ damageCalculator.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function DamageCalculator(props) {
+    var attacks = props.attacks;
+    var hitchance = 1-(props.hitchance-1)/6;
+    var woundchance = 1-(props.woundchance-1)/6;
+    var savechance = (props.savechance-1)/6;
+
+    var wounds = attacks*hitchance*woundchance*savechance;
+    var woundPercent = wounds/ attacks;
+    var sigma = Math.sqrt(wounds*(1-woundPercent));
+    var negativeSigma = wounds - sigma;
+    var positiveSigma = wounds + sigma;
+
+
+    return (
+            <>
+            <Text>The expected unsaved wounds is: {wounds.toFixed(2)} </Text>
+            <Text>The percentage of attacks going through is: {(woundPercent*100).toFixed(2)}%</Text>
+            <Text>A good chance between {negativeSigma.toFixed(2)} and {positiveSigma.toFixed(2)} wounds</Text>
+            </>
+    )
+};
+
+export default DamageCalculator;
